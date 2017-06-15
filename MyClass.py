75b54734b5b5eca68ca8224e39cbd9d385bc966b
Naveen-Zerocool <ilovemybusiness.square@gmail.com>
2017-06-15 11:45:02 +0530

    Few Changes

--- MyClass.py
+++ MyClass.py
@@ -7,14 +7,14 @@ from pytz import timezone
 import time
 
 # You can leave this bit out if you're using a paid PythonAnywhere account
-#proxy_url = "http://proxy.server:3128"
-#telepot.api._pools = {
-#    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-#}
-#telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 # end of the stuff that's only needed for free accounts
 
-bot = telepot.Bot('308765002:AAHM4xMTFoeoaNWOtE4LCAWPg1qbcu7EsXs')
+bot = telepot.Bot('Add Bot API Token Here')
 
 def details():
   import datetime
@@ -182,3 +182,4 @@ print ('Listening ...')
 # Keep the program running.
 while 1:
     time.sleep(10)
+
